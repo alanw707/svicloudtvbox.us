8467b3c85306165af5ce2ea6b307cbed523796c3
Alan Wang <alanw707@gmail.com>
2026-08-31 08:31:05 -0700

    Purge stale production cache

--- theme/svicloudtvbox-lumen/functions.php
+++ theme/svicloudtvbox-lumen/functions.php
@@ -485,7 +485,7 @@ add_filter('woocommerce_add_to_cart_redirect', function (string $url): string {
     return wc_get_checkout_url();
 });
 
-const SVIC_LITESPEED_PURGE_MARK = 'svic-15p-second-gallery-label-refresh-20260824';
+const SVIC_LITESPEED_PURGE_MARK = 'svic-disable-google-promo-yogurt-guide-20260831';
 const SVIC_REWRITE_FLUSH_MARK   = 'svic-rewrite-flush-20260407';
 
 add_action('init', function () {
@@ -498,7 +498,7 @@ add_action('init', function () {
     }
 
     update_option('svic_litespeed_last_purge', SVIC_LITESPEED_PURGE_MARK, false);
-    \LiteSpeed\Purge::purge_all('SVIC 15P remote gallery refresh');
+    \LiteSpeed\Purge::purge_all('SVIC disable Google promo and refresh Yogurt Mo guide');
 }, 1);
 
 // One-time rewrite flush to ensure alias rewrites (e.g., /compare/) take effect.
